fddbe7b01e4cb9c8d0ede84ab6fcde6a9b4d5c9a
venkatesh <venkateshjatavath2@gmail.com>
2025-07-16 23:06:11 +0530

    new code

---
+++ print.py
@@ -0,0 +1,13 @@
+num_of_prod = 10 # this is variable 1
+price_per_prod = 50 
+age = 23
+dob = 2001
+total_cost = num_of_prod * price_per_prod
+print ("5+3=",5+3)
+print ("price of each pen=", price_per_prod)
+print ("total_cost=",total_cost)
+print ("my name is venkatesh having",age,"born in",dob)
+
+#format string 
+
+print (f"my name is venkatesh having {age} born in {dob}")
