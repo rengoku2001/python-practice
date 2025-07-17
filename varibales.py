e6d5d00f748bb3763add1e781c14b290765c78ab
venkatesh <venkateshjatavath2@gmail.com>
2025-07-17 22:32:30 +0530

    new file

---
+++ varibales.py
@@ -0,0 +1,16 @@
+value1 = 50 #integers
+#type is a function python which gives the type of the data 
+print (type(value1)) #here print and type both are functions #function inside function means first inside function execute
+
+value1 = 90.1 #float
+print (type(value1)) #in python mutiple varibales are allowed
+
+value1 = 4 + 5j #complex
+print (type(value1))
+print(value1 * value1) # (4+5j) * (4+5j)
+
+value1 = "hello" #any text is string
+print (type(value1))
+
+value1 = True #boolean just 2 values = true or false
+print(type(value1))
