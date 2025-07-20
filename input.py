11a65a84cbb9c2206a193b1dc48cc33a321c0859
venkatesh <venkateshjatavath2@gmail.com>
2025-07-20 17:28:37 +0530

    commit

---
+++ input.py
@@ -0,0 +1,14 @@
+value1 = 50
+#to get the input from the user - input()
+name=input("Enter your name : ")
+mail=input("Give your mail : ")
+print("hello",name)
+# input will take value as string 
+num1=input("Enter a number : ")
+num1=int(num1) #here we converted num1 in integers
+num2=int(input("Enter another number : "))  #here function inside function conversion
+# convert anything int() , float() , str() , bool() , complex() 
+sum=num1+num2
+print("Sum of two values entered is",sum)
+
+
