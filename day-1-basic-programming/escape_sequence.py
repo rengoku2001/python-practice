bfe33a353c16768d70937e4e511978b3d7dc485a
venkatesh <venkateshjatavath2@gmail.com>
2025-07-27 21:27:46 +0530

    changes

---
+++ day-1-basic-programming/escape_sequence.py
@@ -0,0 +1,8 @@
+print("hello how are you ?")
+# \n is used to give a new line in python
+print("\n is used to give a new line in python")
+print("what's your name")
+
+print('what\'s your name') #here in to escape single quotation I can use "\"
+
+print("my name is venkatesh");print("and other name is venk") # ";" this semi colon is used to run in same line
